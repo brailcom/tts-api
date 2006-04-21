48c79fccf558de735ca8a94c2d509114bc28d903
hanke <hanke>
2006-04-21 16:21:47 +0000

    Use texi2dvi instead of TeX.

--- gen-doc.sh
+++ gen-doc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 makeinfo tts-api.texi &&
-makeinfo tts-api.texi -E tts-api-m.texi && texi2dvi tts-api-m.texi
+makeinfo tts-api.texi -E tts-api-m.texi && texi2dvi tts-api-m.texi &&
+makeinfo -o tts-api.html --html tts-api.texi
