707001bc5de6e6262b2351baf73e5677bf0a703d
hanke <hanke>
2006-05-17 11:18:24 +0000

    *** empty log message ***

--- gen-doc.sh
+++ gen-doc.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# It seems the current version of texi2dvi ignores the -o
+# parameter, so we need to copy manually
+
 makeinfo tts-api.texi &&
 makeinfo tts-api.texi -E tts-api-m.texi &&
-texi2dvi -o tts-api.dvi tts-api-m.texi &&
-texi2dvi --pdf -o tts-api.dvi tts-api-m.texi &&
+texi2dvi tts-api-m.texi &&
+texi2dvi --pdf tts-api-m.texi &&
+cp tts-api-m.dvi tts-api.dvi &&
+cp tts-api-m.pdf tts-api.pdf &&
 makeinfo -o tts-api.html --html --no-split tts-api.texi
