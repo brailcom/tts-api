d8aed5bf04d3bc7f6089fc782f786d1e81e40767
hanke <hanke>
2006-05-17 10:44:39 +0000

    *** empty log message ***

--- gen-doc.sh
+++ gen-doc.sh
@@ -2,4 +2,5 @@
 makeinfo tts-api.texi &&
 makeinfo tts-api.texi -E tts-api-m.texi &&
 texi2dvi -o tts-api.dvi tts-api-m.texi &&
-makeinfo -o tts-api.html --html tts-api.texi
+texi2dvi --pdf -o tts-api.dvi tts-api-m.texi &&
+makeinfo -o tts-api.html --html --no-split tts-api.texi
